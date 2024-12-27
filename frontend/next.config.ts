dc76dda3b57b90752c0b8e23195551524d09c780
jaeri <jaeriadias@gmail.com>
2024-12-27 20:47:54 +0100

    frontend files 0.1

---
+++ frontend/next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  productionBrowserSourceMaps: false, // Disable source maps
+};
+
+export default nextConfig;
